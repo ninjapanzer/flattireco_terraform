8affa0e01b5770ddb1efedb7fe1d223263f9427a
Paul Scarrone <paul.scarrone@gmail.com>
2019-10-06 12:14:28 -0400

    add range key for composit dynamo composit key

--- modules/dynamodb/auth_table/main.tf
+++ modules/dynamodb/auth_table/main.tf
@@ -2,9 +2,15 @@ resource "aws_dynamodb_table" "auth_table" {
   name           = "authTable"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "Provider"
+  range_key      = "TokenType"
 
   attribute {
     name = "Provider"
+    type = "S"
+  }
+
+  attribute {
+    name = "TokenType"
     type = "S"
   }
 
